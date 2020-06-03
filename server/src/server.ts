69932d6a3b0133c43700039feccccfb87013c658
agftech <50276045+agftech@users.noreply.github.com>
2020-06-02 22:07:59 -0300

    #Second day of NLW
    Adding migrations, seeds...
    Folder uploads: database, controllers, uploads.
    File uploads: routes, server, knexfile, among others.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,12 @@
-import express from 'express';
+import express from "express";
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-    //response.send('Hello World');
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Nóis de novo aqui'
-        ]);
-    
-});
-
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333);
